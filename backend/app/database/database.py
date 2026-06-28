16c9c61c074e73cb506a598e83ec54954a492a7d
Gaurav Aawanke <your-email@example.com>
2026-06-29 00:30:37 +0530

    save resume data and all  information in database

--- backend/app/database/database.py
+++ backend/app/database/database.py
@@ -1,10 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "postgresql+psycopg2://postgres:Gaurav%401234@localhost:5432/financial_health"
 engine = create_engine(DATABASE_URL)
 
 Base = declarative_base()
+
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 # conn = get_db_connection()
 # print("database connected suesfully")
--- backend/app/routers/upload_resume.py
+++ backend/app/routers/upload_resume.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, UploadFile, File, HTTPException
 import os
-
+from pypdf import PdfReader
 from core.logger import logger
-
+from database.database import SessionLocal
+from models.resume import Resume
 router = APIRouter(
     tags=["Upload Resume"]
 )
@@ -20,7 +21,6 @@ async def upload_resume(file: UploadFile = File(...)):
 
     if ext not in ALLOWED_EXTENSIONS:
         logger.warning(f"Invalid file type: {file.filename}")
-
         raise HTTPException(
             status_code=415,
             detail="Only PDF files are allowed."
@@ -30,12 +30,45 @@ async def upload_resume(file: UploadFile = File(...)):
 
     save_path = os.path.join(file_save_path, file.filename)
 
-    logger.info(f"Saving file: {file.filename}")
+    logger.info(f"Saving resume: {file.filename}")
 
     with open(save_path, "wb") as f:
         f.write(contents)
 
-    logger.info(f"Resume uploaded successfully: {file.filename}")
+    logger.info("Resume saved successfully.")
+
+    logger.info("Extracting text from PDF.")
+
+    reader = PdfReader(save_path)
+
+    text = ""
+
+    for page in reader.pages:
+        page_text = page.extract_text()
+        if page_text:
+            text += page_text
+
+    logger.info(f"Text extraction completed. Characters extracted: {len(text)}")
+    db = SessionLocal()
+
+    resume = Resume(
+        file_name=file.filename,
+        file_data=contents
+    )
+
+    db.add(resume)
+    db.commit()
+    db.refresh(resume)
+
+    logger.info(f"Resume saved to database. Resume ID: {resume.id}")
+
+    db.close()
+    
+    
+
+
+
+    # print(text)
 
     return {
         "filename": file.filename,
