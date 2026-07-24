632c42e980abaad224ca2bf681c5f0a661f89473
Gaurav Aawanke <your-email@example.com>
2026-07-25 02:05:25 +0530

    feat: integrate knowledge base with role-aware resume analysis

--- backend/app/routers/upload_resume.py
+++ backend/app/routers/upload_resume.py
@@ -60,21 +60,25 @@ async def upload_resume(file: UploadFile = File(...)):
 
     with open(save_path, "wb") as f:
         f.write(contents)
+        logger.info(f"Resume saved at: {save_path}")
 
     logger.info(f"Resume uploaded successfully: {file.filename}")
 
     reader = PdfReader(save_path)
+    logger.info("Reading uploaded PDF.")
 
     text = ""
 
     for page in reader.pages:
         page_text = page.extract_text()
+        
         if page_text:
             text += page_text
 
     logger.info(f"Resume text extracted ({len(text)} characters).")
 
     db = SessionLocal()
+    logger.info("Opening database session.")
 
     try:
         # ---------------------------
@@ -84,69 +88,92 @@ async def upload_resume(file: UploadFile = File(...)):
             file_name=file.filename,
             file_data=contents
         )
-
+        
+        logger.info("Saving resume to database.")
         db.add(resume)
         db.commit()
+        
         db.refresh(resume)
-
         logger.info(f"Resume stored in database. Resume ID: {resume.id}")
-
+        
         # ---------------------------
         # AI Analysis
         # ---------------------------
         logger.info("Sending resume to Qwen for analysis.")
         
-        selected_role = "Backend Developer"   # Temporar
+        # selected_role = "Backend Developer"   # Temporar
     
-        role_id = 1
+        role_id = 5
+        
+        logger.info(f"Fetching role with ID: {role_id}")
         role = db.query(Role).filter(Role.role_id == role_id).first()
         if not role:
+            logger.warning(f"Role not found for role_id={role_id}")
             raise HTTPException(status_code=404 , detail="Role Not Found")
         
-        selected_role = role.role_name                    
+        selected_role = role.role_name      
+        logger.info(f"Role selected: {selected_role}")              
         BASE_DIR = Path(__file__).resolve().parent.parent
         KNOWLEDGE_DIR = BASE_DIR / "knowledge"
         
+        
         knoweldge_file_path = KNOWLEDGE_DIR / role.knowledge_file
+        
+        print(knoweldge_file_path)
+        print(knoweldge_file_path.exists())
                 
         if not knoweldge_file_path.exists():
+            logger.warning(f"Knowledge file not found: {knoweldge_file_path}")
             raise HTTPException(
                 status_code=404, 
                 detail=f"Knowledge file '{role.knowledge_file}' not found."
             )
             
+            
+        logger.info(f"Loading knowledge file: {knoweldge_file_path}")    
+            
         reader1 = PdfReader(knoweldge_file_path)
+        logger.info("Reading knowledge PDF.")        
         
         text1 = ""
         
         
         for i, page in enumerate(reader1.pages):
-            print("Reading page", i + 1)
-
+            logger.info(f"Reading knowledge PDF page {i + 1}")
             page_text = page.extract_text()
+            
+            logger.info(f"Resume contains {len(reader1.pages)} pages.")
 
-            print("Extracted:", page_text is not None)
+            # print("Extracted:", page_text is not None)
+            logger.info(f"Text extracted from page {i + 1}: {page_text is not None}")
 
             if page_text:
                 text1 += page_text
 
-        print("Final Length:", len(text1))
-
+        # print("Final Length:", len(text1))
+        
+        logger.info(f"Knowledge text extracted successfully. Total characters: {len(text1)}")
+        
+        
         text_splitter  = RecursiveCharacterTextSplitter(
             chunk_size = 100,
             chunk_overlap = 0
         )
         
-        logger.info("Splitting text")
+        logger.info("Splitting knowledge into chunks.") 
+        
+               
         
         split_texts = text_splitter .split_text(text1)
         
-        logger.info(f"Total chunks: {len(split_texts)}")
-
+        logger.info(f"Total chunks created: {len(split_texts)}")
+        
+        logger.info("Generating embeddings for knowledge chunks.")
+        
         genrate_embedding = embedding(split_texts)
+        logger.info("Knowledge embeddings generated successfully.")
         
-        logger.info(f"generating embeddings for knowledge chunks,")
-        
+        logger.info("Storing embeddings in ChromaDB.")        
         question_searching = collection.add(
                 ids=[str(uuid4()) for _ in split_texts],
                 documents=split_texts,
@@ -162,40 +189,33 @@ async def upload_resume(file: UploadFile = File(...)):
 
         logger.info("Knowledge stored in ChromaDB.")
 
-        logger.info(f"store in vector database")
+        logger.info("Knowledge embeddings stored successfully in vector database.")
         
-        print("Collection Count:", collection.count())
-        
+        logger.info("Generating resume embedding.")
         resume_embedding = embedding([text])[0]
-        print("reume building-======================" , resume_embedding)
+        logger.info("Resume embedding generated successfully.")       
+        logger.info("Searching relevant knowledge from ChromaDB.") 
         results = collection.query(
             query_embeddings= [resume_embedding],
             n_results=5
         )
         
         retrieved_docs = results["documents"][0]
-
-        role_knowledgesss = "\n\n".join(retrieved_docs)
-
-        print("\nRetrieved Knowledge\n")
-        print(role_knowledgesss)
-
-       
         
-        
-        
-       
-
-        print("===============reterival===============", results)
-                
+        logger.info(f"Retrieved {len(retrieved_docs)} relevant knowledge chunks.")
+        role_knowledge = "\n\n".join(retrieved_docs)
+        logger.info("Knowledge retrieval completed successfully.")                
             
-        role_knowledge=""
+        logger.info("Sending prompt to Qwen LLM.")
+        
         analysis = ask_llm(
             resume_text=text,
             selected_role=selected_role,
             role_knowledge=role_knowledge)
         
+        
         analysis = clean_json_response(analysis)
+        logger.info("LLM response cleaned successfully.")
         
 
         logger.info("AI analysis completed.")
@@ -212,6 +232,8 @@ async def upload_resume(file: UploadFile = File(...)):
         # ---------------------------
         # Save Analysis
         # ---------------------------
+        
+        
         analysis_db = Analysis(
             resume_id=resume.id,
             ats_score=analysis["ats_score"],
@@ -253,19 +275,31 @@ async def upload_resume(file: UploadFile = File(...)):
     for item in analysis["suggestions"]
 ),    
         )
+        
+        logger.info("Saving analysis to database.")
 
         db.add(analysis_db)
         db.commit()
         db.refresh(analysis_db)
+        
+        
         resume_id = resume.id
         analysis_id = analysis_db.id
 
         logger.info(
             f"Analysis stored in database. Analysis ID: {analysis_db.id}"
         )
+    
+    except Exception as e:
+        logger.error(
+        f"Error while processing resume: {str(e)}",
+        exc_info=True
+    )
+        raise
 
     finally:
-        db.close()
+        logger.info("Closing database session.")
+        db.close()    
 
     return {
     "message": "Resume uploaded and analyzed successfully.",
@@ -281,11 +315,7 @@ async def upload_resume(file: UploadFile = File(...)):
     "suggested_projects":analysis["suggested_projects"],
     "estimated_timeline":analysis["estimated_timeline"],
     "action_plan":analysis["action_plan"],
-    
-    
-    
-    
-    
+       
     
 }
     
